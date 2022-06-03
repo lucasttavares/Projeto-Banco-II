90f06bd531fc5640ed5c484c4e27d51726390ee1
Lucas Tavares <zlucas.np@gmail.com>
2022-06-03 16:06:08 -0300

    feat: salva calores no banco

--- controllers/PontoController.js
+++ controllers/PontoController.js
@@ -4,11 +4,14 @@ const Ponto = require('../models/ponto');
 const addPonto = async (request, response) =>{
 
     const nome = request.body.nome;
+    const valor = request.body.valor;
+    const org = request.body.org;
+    const data = request.body.data;
     const geometria = {type: 'Point', coordinates:[request.body.lat, request.body.lng]}
 
     console.log(geometria);
-
-    const ponto = Ponto.build({nome, geometria});
+    sincronizar()
+    const ponto = Ponto.build({nome, valor, org, data, geometria});
     ponto.save().then(()=>{
         response.status(200).send('Ponto salvo!');
     }).catch(err =>{
@@ -18,8 +21,9 @@ const addPonto = async (request, response) =>{
 };
 
 const sincronizar = async(request, response) =>{
-    await Ponto.sync();
-    response.status(200).send('Sincronizado');
+    await Ponto.sync()
 }
+
+
 
 module.exports = {addPonto, sincronizar};
