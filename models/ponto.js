dabcc129538ba26e07e93d7a2ad640439dfcbc5a
Lucas Tavares <zlucas.np@gmail.com>
2022-06-03 16:06:48 -0300

    feat: cria colunas no banco

--- models/ponto.js
+++ models/ponto.js
@@ -1,14 +1,28 @@
+const { DataTypes } = require('sequelize');
 const Sequelize = require('sequelize');
 const database = require('../database/database');
+/* const query = `select ST_x(geometria) as lat, select ST_y(geometria) as lng from "pontos"` */
 
 const Ponto = database.define('ponto',{
     id:{
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
         primaryKey: true
     },
     nome:{
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    valor:{
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    org:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    data:{
+        type: DataTypes.STRING,
         allowNull: false
     },
     geometria:{
