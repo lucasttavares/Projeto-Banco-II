17e44b5323d5fe8b631d85de1ae48cc91c19f114
Lucas Tavares <zlucas.np@gmail.com>
2022-06-10 16:41:54 -0300

    feat: prepara ambiente para usar handlebars e organiza portas da aplicação

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const handlebars = require('express-handlebars');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 const port = process.env.API_PORT;
 
+app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
 app.listen(port, ()=>{
     console.log(`App running on port ${port}.`);
 });
+
+app.use(express.static('assets/'))
+
+app.use(express.urlencoded({extended: false}))
 
 const pontoController = require('./controllers/PontoController');
 
 app.post('/pontos', pontoController.addPonto);
 
-app.get('/', pontoController.sincronizar);
+app.get('/', pontoController.getPage);
+
+app.post('/teste', pontoController.getPontos)
