9aab3ae4e270faf12b7560ce1dd791d520f6929b
Lucas Tavares <zlucas.np@gmail.com>
2022-06-10 18:10:23 -0300

    fix: ajuste no banco

--- database/database.js
+++ database/database.js
@@ -5,7 +5,8 @@ const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USERNAME, process.env.PG_PASSWORD, {
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
-  dialect: 'postgres'
+  dialect: 'postgres',
+  query:{raw:true}
 });
 
 module.exports = sequelize;
