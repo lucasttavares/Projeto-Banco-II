ce2b7c5def3e6b462860a47c0f32eab55abb2c7d
Lucas Tavares <zlucas.np@gmail.com>
2022-06-13 13:19:11 -0300

    view: ajustes nos assets

--- assets/javascript/mapa.js
+++ assets/javascript/mapa.js
@@ -7,7 +7,7 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: center,
     zoom: 14,
-    mapTypeId: 'satellite'
+    mapTypeId: 'roadmap'
   });
 
   marker = new google.maps.Marker({
@@ -48,27 +48,3 @@ function salvar(){
     .catch(error => alert('Falha ao salvar!'));    
 
 }
-
-/* let map1;
-let marker1;
-
-let center1 = {lat: -6.888463202449027, lng: -38.558930105104125};
-
-function initMap() {
-  map1 = new google.maps.Map(document.getElementById("map1"), {
-    center1: center1,
-    zoom: 14,
-    mapTypeId: 'satellite'
-  });
-
-  marker1 = new google.maps.Marker({
-      map1: map,
-      position: center1,
-      draggable: true,
-      animation: google.maps.Animation.DROP
-  });
-}
-
-function addMarker(evt){
-    marker1.setPosition(evt.latLng);
-} */
